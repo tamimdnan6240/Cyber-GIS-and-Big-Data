273e62fe142ddf009d012064202984bd889e0362
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2023-03-25 16:03:38 -0400

    Update files.sh

--- LAB2/files.sh
+++ LAB2/files.sh
@@ -1,1 +1,3 @@
-lab files
+$ is important in bash to denote a string as a variable in an arithmatic equations. 
+For, example, folders_per_cpu = $((500/n)) means n is a variable here, if you omit $ from this equation
+then you will not get n as a variable in the arithmatic equation. 
